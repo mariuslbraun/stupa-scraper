edd3afe56e706f13286788ba86f182a6a66cc278
mariuslbraun <marius.braun-2@wirtschaft.uni-giessen.de>
2023-07-11 16:03:17 +0200

    set up renv

---
+++ setup.R
@@ -0,0 +1,28 @@
+# This script sets up a renv for the project and installs required packages
+
+# We are using the daily CRAN snapshots from RStudio Package Manager: 
+# https://packagemanager.rstudio.com/client/#/repos/1/overview
+# Currently, we are using the snapshot from July 6, 2023:
+# https://packagemanager.posit.co/cran/2023-07-06
+
+# Select the repo snapshot:
+options(repos = c(
+  REPO_NAME = "https://packagemanager.posit.co/cran/2023-07-06"
+  ))
+
+# Install renv
+install.packages("renv")
+
+# Initialize renv for the project
+# bare = TRUE: instead of installing dependencies automatically, we install packages manually
+renv::init(bare = TRUE)
+
+# Install the packages
+install.packages(c(
+  "weed", "openxlsx", "sf", "rgdal", "ggmap", "tidyverse",
+  "dplyr", "stringr", "tidyr", "ggplot2", "spdep", "splm", "mice",
+  "stargazer", "broom"
+  ))
+
+# Take a snapshot of the renv
+renv::snapshot()
